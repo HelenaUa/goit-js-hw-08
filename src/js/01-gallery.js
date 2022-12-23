369f13e46684e587d91c178c6d069c792fc9ad07
HelenaUa <delena.1408@gmail.com>
2022-12-23 21:26:31 +0200

    add task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
 
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+// 1.Добавь библиотеку SimpleLightbox как зависимость проекта используя
+// npm(ссылка на CDN из твоей прошлой работы больше не нужна).
+// 2.Используй свой JavaScript код из предыдущей домашней работы, но выполни рефакторинг с учетом того,
+// что библиотека была установлена через npm(синтаксис import /export).
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+const arrayDiv = createGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", arrayDiv);
+
+function createGalleryMarkup(array) {
+//     return array.map(({ preview, original, description }) => `<a class="gallery__item"
+//  href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       alt="${description}"
+//     />
+//   </a>`).join(" ");
+    return array.reduce((acc, {preview, original, description}) => acc + `<a class="gallery__item"
+  href="${original}">
+   <img
+       class="gallery__image"
+       src="${preview}"
+       alt="${description}"
+     />
+   </a>`, '')
+};
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+  captionDelay: 250,
+});
+ 
+
+    
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,48 @@
+// В HTML есть < iframe > с видео для Vimeo плеера.Напиши скрипт который будет сохранять текущее время 
+// воспроизведения видео в локальное хранилище и, при перезагрузке страницы, продолжать воспроизводить 
+// видео с этого времени.
 
+// Разбей его на несколько подзадач:
+// 1.Ознакомься с документацией библиотеки Vimeo плеера.
+// 2.Добавь библиотеку как зависимость проекта через npm.
+// 3.Инициализируй плеер в файле скрипта как это описано в секции pre - existing player, но учти что 
+// у тебя плеер добавлен как npm пакет,а не через CDN.
+// 4.Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
+// 5.Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка 
+// "videoplayer-current-time".
+// 6.При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение 
+// с сохраненной позиции.
+// 7.Добавь в проект библиотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище 
+// не чаще чем раз в секунду.
+
+import Player from '@vimeo/player';
+console.log(Player);
+
+ const iframe = document.querySelector('iframe');
+ const player = new Vimeo.Player(iframe);
+
+//     player.on('play', function() {
+//         console.log('played the video!');
+//     });
+
+//     player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
+
+//     const onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+player.on('eventName', function(data) {
+    const data = {
+    duration: 61.857,
+    percent: 0.049,
+    seconds: 3.034,
+}
+});
+
+
+
+// data is an object containing properties specific to that event
